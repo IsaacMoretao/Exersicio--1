bcc68222d02b87732d87835cd3669efe853ec0fb
Isaac Moretão <isaachanam@gmail.com>
2022-01-20 11:37:14 -0300

    comit

--- add.js
+++ add.js
@@ -17,7 +17,7 @@
 
    function inlista(n, l) {
 
-    if(l.indexOf(Number(n)) != -1) {
+    if (l.indexOf(Number(n)) != -1) {
       return true
     } else{
       return false
@@ -25,24 +25,24 @@
     
   }
    
-
-
    if (num.value <= 0) {
-     // alerta com valor do campo
      window.alert(`[ERRO] O numero ${num.value} não é válido [ERRO]`);
 
    } else if (num.value > 100) {
-     // alerta com valor do campo
      window.alert(`[ERRO] O numero ${num.value} não é válido [ERRO]`);
 
-   }  else if (isNumero(num.value) && !inlista(num.value, valores)) {
-     window.alert(`O numero ${num.value} já está na lista`)
+   }
+   
+   if (isNumero(num.value) || !inlista (num.value, valores)) {
 
+    valores.push(Number(num.value))
+    let item = document.createElement('option')
+    item.text = `valor ${num.value} adicionado`
+    lista.appendChild(item)
+    
     } else {
-     // alerta com valor do campo
-     window.alert("tudo certo!");
+     window.alert(`O valor ${num.value} Já está na lista`);
 
    }
-
 
  }
